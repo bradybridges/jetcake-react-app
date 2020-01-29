0aa566bef900455bccc0772c56107ad02327fc29
brady <bradyjbridges@gmail.com>
2020-01-29 15:59:22 -0700

    Update testing to reflect changes and new methods

---
+++ src/Components/Login/Login.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Login from './Login';
+import { shallow } from 'enzyme';
+
+describe('Login', () => {
+  let wrapper;
+  const mockToggleShowLogin = jest.fn();
+  
+  beforeEach(() => {
+    wrapper = shallow(<Login toggleShowLogin={mockToggleShowLogin} />);
+  });
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('handleChange should update state of email or password', () => {
+    const mockEmailEvent = {
+      target: { name: "email", value: "test@test.com" },
+    };
+    const mockPasswordEvent = {
+      target: { name: "password", value: "secretpassword" },
+    };
+    expect(wrapper.state()).toEqual({ email: "", password: "", error: null });
+    wrapper.instance().handleChange(mockEmailEvent);
+    expect(wrapper.state('email')).toEqual(('test@test.com'));
+    wrapper.instance().handleChange(mockPasswordEvent);
+    expect(wrapper.state('password')).toEqual('secretpassword');
+  });
+})
