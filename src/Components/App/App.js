8033af6af28d7348fdaca37c86d5b017a531940c
brady <bradyjbridges@gmail.com>
2020-01-29 14:08:49 -0700

    Add state to hold user and login status, change to class component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,27 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 import { Route, Switch } from 'react-router-dom';
 import { Header } from '../Header/Header';
+import Login from '../Login/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/">
-          <Header />
-        </Route>
-        <Route path="/create">
-          <h1>Create An Account</h1>
-        </Route>
-        <Route path="/login">
-          <h1>Login</h1>
-        </Route>
-        <Route path="/profile">
-          <h1>Profile</h1>
-        </Route>
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  state = {
+    showLogin: false,
+    user: null,
+  }
+
+  toggleShowLogin = () => {
+    const toggledState = !this.state.showLogin;
+    this.setState({ showLogin: toggledState });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Header toggleShowLogin={this.toggleShowLogin} />
+            {this.state.showLogin && <Login toggleShowLogin={this.toggleShowLogin} />}
+          </Route>
+          <Route path="/create">
+            <h1>Create An Account</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login</h1>
+            <Login />
+          </Route>
+          <Route path="/profile">
+            <h1>Profile</h1>
+          </Route>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
