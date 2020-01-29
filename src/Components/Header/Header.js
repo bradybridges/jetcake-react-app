1f553cbd8ba69bebf8711854996f1c31f7afca4d
brady <bradyjbridges@gmail.com>
2020-01-29 15:56:39 -0700

    Add dynamic logout button

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Header.scss';
 import { Link } from 'react-router-dom';
 
-export const Header = ({toggleShowLogin}) => {
+export const Header = ({toggleShowLogin, user, handleLogout}) => {
   return (
     <header>
       <h1 id="main-header">Hero Page</h1>
@@ -11,6 +11,7 @@ export const Header = ({toggleShowLogin}) => {
           <li className="nav-link"><span className="link" onClick={toggleShowLogin}>Login</span></li>
           <li className="nav-link"><Link className="link" to="/create">Create Account</Link></li>
           <li className="nav-link"><Link className="link" to="/profile">View Profile</Link></li>
+          { user && <span className="link" onClick={handleLogout}>Logout</span>}
         </ul>
       </nav>
     </header>
