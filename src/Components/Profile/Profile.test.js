065cc017af341d43b7a06cfb266f194f1bf6cc6d
brady <bradyjbridges@gmail.com>
2020-01-30 13:30:59 -0700

    Add testing for Profile component

---
+++ src/Components/Profile/Profile.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Profile from './Profile';
+import { shallow } from 'enzyme';
+
+describe('Profile', () => {
+  let wrapper;
+  const mockHandleUpdateProfile = jest.fn();
+  it('should match the snapshot when edit profile is false', () => {
+    wrapper = shallow(<Profile 
+      email="test@test.com"
+      address="123 test blvd"
+      phone="1112223333"
+      dob="1990-10-10"
+      securityOne="Roger"
+      securityTwo="Fido"
+      securityThree="Minneapolis"
+      photoURL="https://mockHost.com/userID/profile.jpg"
+      handleUpdateProfile={mockHandleUpdateProfile}
+    />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should match the snapshot when edit profile is true', () => {
+    let editTrueWrapper = shallow(<Profile 
+      email="test@test.com"
+      address="123 test blvd"
+      phone="1112223333"
+      dob="1990-10-10"
+      securityOne="Roger"
+      securityTwo="Fido"
+      securityThree="Minneapolis"
+      photoURL="https://mockHost.com/userID/profile.jpg"
+      handleUpdateProfile={mockHandleUpdateProfile}
+    />);
+    editTrueWrapper.setState({ showEditProfile: true });
+    expect(editTrueWrapper).toMatchSnapshot();
+  })
+
+  it('toggleShowEditProfile should toggle showEditProfile in state', () => {
+    expect(wrapper.state('showEditProfile')).toEqual(false);
+    wrapper.instance().toggleShowEditProfile();
+    expect(wrapper.state('showEditProfile')).toEqual(true);
+  });
+})
