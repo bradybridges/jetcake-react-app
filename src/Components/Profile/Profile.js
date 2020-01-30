427dc913cdc03fcfa883676a2403941e300a6e40
brady <bradyjbridges@gmail.com>
2020-01-30 12:15:34 -0700

    Convert component to class based

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,48 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ProfileInformation from '../ProfileInformation/ProfileInformation';
+const editImg = require('../../Assets/edit.png');
 
-export default function Profile({ email, address, phone, dob, securityOne, securityTwo, securityThree, photoURL }) {
-  return (
-    <section id="profile-container">
-      <h2>Profile</h2>
-      <img src={photoURL} alt="user profile" />
-      <section id="email-container">
-        <label>Email</label>
-        <p>{email}</p>
-        <button>Change</button>
+export default class Profile extends Component{
+  render() {
+    const {
+      email, 
+      address, 
+      phone, 
+      dob, 
+      securityOne, 
+      securityTwo, 
+      securityThree, 
+      photoURL, 
+      handleUpdateProfile 
+    } = this.props;
+    return (
+      <section id="profile-container">
+        <img id="profile-img" src={photoURL} alt="profile" />
+        <div id="edit-btn-container" style={{backgroundColor: 'black'}}>
+          <img src={editImg} alt="edit" />
+        </div>
+        <ProfileInformation 
+          email={email}
+          address={address}
+          phone={phone}
+          dob={dob}
+          securityOne={securityOne}
+          securityTwo={securityTwo}
+          securityThree={securityThree}
+        />
       </section>
-      <section id="address-container">
-        <label>address</label>
-        <p>{address}</p>
-        <button>Change</button>
-      </section>
-      <section id="phone-container">
-        <label>Phone</label>
-        <p>{phone}</p>
-        <button>Change</button>
-      </section>
-      <section id="date-of-birth-container">
-        <label>Date of Birth</label>
-        <p>{dob}</p>
-        <button>Change</button>
-      </section>
-      <section id="security-one-container">
-        <p>Mother's maiden name?</p>
-        <label>Answer</label>
-        <p>{securityOne}</p>
-        <button>Change</button>
-      </section>
-      <section id="security-two-container">
-        <p>Name of first pet?</p>
-        <label>Answer</label>
-        <p>{securityTwo}</p>
-        <button>Change</button>
-      </section>
-      <section id="email-container">
-        <p>Town where you grew up?</p>
-        <label>Answer</label>
-        <p>{securityThree}</p>
-        <button>Change</button>
-      </section>
-    </section>
-  )
+    );
+  }
 }
+
+{/* <table>
+<tbody>
+  <tr>
+    <td>Email</td>
+    <td>{email}</td>
+  </tr>
+  <tr>
+    <td>Address</td>
+    <td>{address}</td>
+  </tr>
+  <tr>
+    <td>Phone Number</td>
+    <td>{phone}</td>
+  </tr>
+  <tr>
+    <td>Birthday</td>
+    <td>{dob}</td>
+  </tr>
+  <tr>
+    <td>Mother's maiden name?</td>
+    <td>{securityOne}</td>
+  </tr>
+  <tr>
+    <td>Name of first pet?</td>
+    <td>{securityTwo}</td>
+  </tr>
+  <tr>
+    <td>City where you grew up?</td>
+    <td>{securityThree}</td>
+  </tr>
+</tbody>
+</table> */}
