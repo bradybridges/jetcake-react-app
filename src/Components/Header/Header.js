1771826000dccae449e49525df04263100176fd1
brady <bradyjbridges@gmail.com>
2020-01-30 17:13:12 -0700

    Add dynamic rendering for links depending on user state

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,10 +8,10 @@ export const Header = ({
   <header>
     <h1 id="main-header">Hero Page</h1>
     <section id="account-container">
-      <button className="account-btn" onClick={toggleShowLogin}>Login</button>
-      <button className="account-btn" onClick={toggleShowCreateAccount}>Create Account</button>
-      { user && <button className="account-btn" onClick={handleLogout}>Logout</button> }
+      { !user && <button className="account-btn" onClick={toggleShowLogin}>Login</button>}
+      { !user && <button className="account-btn" onClick={toggleShowCreateAccount}>Create Account</button>}
       { user && <Link className="account-btn" to="/profile">View Profile</Link> }
+      { user && <button className="account-btn" onClick={handleLogout}>Logout</button> }
     </section>
   </header>
 );
