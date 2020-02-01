0da0576efacaabe34fb5c4caa8a6a5c84d98cf1d
brady <bradyjbridges@gmail.com>
2020-02-01 12:54:56 -0700

    Add env variables for deployment

---
+++ src/ApiKeys.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const ApiKeys = {
+  FirebaseConfig: {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+  },
+};
+
+export default ApiKeys;
